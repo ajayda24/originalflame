5b2ad1645347039a979fac4d961a77b4cf4494a8
Ajay Daniel Trevor <59312969+ajayda24@users.noreply.github.com>
2020-07-06 15:52:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function loveFinder(){
+var n=document.form.first.value;
+    var m=document.form.second.value;
+    var p=(n.length+m.length);
+    var f="flames";
+    
+function posDelete(pos,name){
+    
+
+    var t=name.length;
+    if(t>1)
+    {
+        
+    
+        var t=name.length;
+        if(pos <= t)
+        {
+            name = name.slice(0,pos-1) + name.slice(pos);
+            alert(name);
+            var t=name.length;
+            var f=name;
+
+        }
+        else
+        {
+            var t=name.length;
+            var pos=pos%t;
+            if(pos!=0)
+            {
+                name = name.slice(0,pos-1) + name.slice(pos);
+                alert(name);
+                var f=name;
+
+            }
+            else
+            {
+                name = name.slice(0,-1);
+                alert(name);
+                var f=name;
+            }
+
+        }
+        
+    }
+    
+    
+    posDelete(p,f);
+}
+posDelete(p,f);
+}
